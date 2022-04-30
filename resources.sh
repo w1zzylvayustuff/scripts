93bde456ada3ac18cd8c0e2be839624e9e78fd59
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-04-30 22:58:25 +0200

    Resources: typo

--- resources.sh
+++ resources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-STUFF=https://github.com/w1zzylvayustuff
-CHALDEA=https://github.com/chaldeaprjkt
+STUFF="https://github.com/w1zzylvayustuff"
+CHALDEA="https://github.com/chaldeaprjkt"
 echo "Cloning resources"
 git clone -b cherish $(STUFF)/device_xiaomi_vayu device/xiaomi/vayu && git clone -b arrow-12.1 $(STUFF)/android_vendor_xiaomi_vayu vendor/xiaomi/vayu && git clone --depth=1 -b main $(CHALDEA)/kernel_xiaomi_vayu kernel/xiaomi/vayu
 echo "Cloning finished"
