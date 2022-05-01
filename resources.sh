dcb50cba87abf4f08e5935ea8c647bb1ce44cb42
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-05-01 20:36:00 +0200

    Resources: Add awaken trees

--- resources.sh
+++ resources.sh
@@ -48,3 +48,19 @@ git clone -b lineage-19.1 https://github.com/LineageOS/android_packages_resource
 git clone --depth=1 -b main https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton
 
 fi
+
+if [ "$respuesta" = "vayu-awaken" ];
+then
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+git clone -b lineage-19.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone -b lineage-19.1 https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
+git clone -b 12.1 https://github.com/Project-Awaken/android_device_xiaomi_vayu.git device/xiaomi/vayu
+git clone -b 12.1 https://github.com/poco-x3p/android_vendor_xiaomi_vayu.git vendor/xiaomi/vayu
+git clone -b 12.1 https://github.com/raghavt20/kernel_sm8150.git kernel/xiaomi/vayu
+git clone -b 12-caf-sm8150 https://github.com/poco-x3p/android_hardware_qcom_audio.git hardware/qcom-caf/sm8150/audio
+git clone -b 12-caf-sm8150 https://github.com/poco-x3p/android_hardware_qcom_display.git hardware/qcom-caf/sm8150/display
+git clone -b 12-caf-sm8150 https://github.com/poco-x3p/android_hardware_qcom_media.git hardware/qcom-caf/sm8150/media
+
+fi
